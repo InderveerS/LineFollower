82619635ceb79698500ed6dbfe6a4bba4e671c5d
InderveerS <inderveer.sidhu@gmail.com>
2026-05-18 15:20:45 -0700

    motors.h made

---
+++ softwareHere/allSoftware/Core/Inc/motors.h
@@ -0,0 +1,24 @@
+<<<<<<< Updated upstream
+=======
+/*
+ * motors.h
+ *
+ *  Created on: May 8, 2026
+ *      Author: inder
+ */
+
+#ifndef INC_MOTORS_H_
+#define INC_MOTORS_H_
+
+void spinCoast(void);
+void spinBrake(void);
+void coastRightMotor(void);
+void coastLeftMotor(void);
+void brakeRightMotor(void);
+void brakeLeftMotor(void);
+void spinPercent(int dutyPercent);
+void spinRightMotor(int dutyPercent);
+void spinLeftMotor(int dutyPercent);
+
+#endif /* INC_MOTORS_H_ */
+>>>>>>> Stashed changes
